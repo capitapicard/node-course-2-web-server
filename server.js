b66452be043ea7bae947c517a105e287827844bb
capitapicard <capitapicard@gmail.com>
2017-05-20 23:31:25 +0200

    Add project page with partials

--- server.js
+++ server.js
@@ -2,19 +2,35 @@
 
 const express = require('express');
 
-//const hbs = require('hbs');
+const hbs = require('hbs');
 const port = process.env.PORT || 3000;
 
 var app = express();
 
-//app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
 
 app.get('/', (req,res) => {
-  res.send('Hello World');
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Welcome to my webSite'
+  });
 });
 
 app.get('/about', (req,res) => {
-//  res.render('about.hbs');
+  res.render('about.hbs', {
+    pageTitle: 'About Page'
+  });
+});
+
+app.get('/projects', (req,res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Projects'
+  });
 });
 
 app.listen(port, () => {
